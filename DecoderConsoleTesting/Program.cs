c0433797d1b2e13bd617021bc775e5cc44fbfd5f
Othon Valentim <othonvvalentim@gmail.com>
2024-10-03 15:55:49 -0300

    fix

--- DecoderConsoleTesting/Program.cs
+++ DecoderConsoleTesting/Program.cs
@@ -4,7 +4,11 @@ using System.IO.Ports;
 Console.WriteLine("Inicializando teste...");
 
 var appSettings = SettingsLoader.LoadSettings();
-var serialPort = new SerialPort(appSettings.SerialPort, appSettings.BaudRate, Parity.None, 8, StopBits.One);
+var serialPort = new SerialPort(appSettings.SerialPort, appSettings.BaudRate, Parity.None, 8, StopBits.One)
+{
+    ReadTimeout = 1000,
+    Encoding = System.Text.Encoding.UTF8
+};
 
 Console.WriteLine("Configurações OK...");
 
